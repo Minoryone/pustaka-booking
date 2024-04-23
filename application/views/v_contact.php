02ba7cb2918862e11dcc9c010f4e60891963f5b4
Minoryone <joshua.christo03@gmail.com>
2024-04-23 11:55:55 +0700

    first commit

---
+++ application/views/v_contact.php
@@ -0,0 +1,13 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Hubungi Kami</h4>
+ <ul type="disc">
+ <li>Facebook : Joshua</li>
+ <li>Instagram : 123joshua</li>
+ </ul>
+ <h4>Call center</h4>
+ <ul type="disc">
+ <li> WA : 081219102874</li>
+ <li> phone : 081219102874</li>
+ </ul>
+</section>
